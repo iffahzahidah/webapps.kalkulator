a116f8794d4b08ab8a51cf8e5502084aa3e06a94
iffahzahidah <iffahzahidah2112@gmail.com>
2024-12-09 10:17:39 +0700

    Update app (2).py

--- app (2).py
+++ app (2).py
@@ -3,19 +3,19 @@ import streamlit as st
 st.title("Kalkulator Sederhana")
 st.write("Aplikasi ini menghitung operasi dasar matematika seperti penjumlahan, pengurangan, perkalian, dan pembagian.")
 
-# Input angka pertama
+#Input angka pertama
 num1 = st.number_input("Masukkan angka pertama:", value=0.0, step=1.0)
 
-# Input angka kedua
+#Input angka kedua
 num2 = st.number_input("Masukkan angka kedua:", value=0.0, step=1.0)
 
-# Pilihan operasi
+#Pilihan operasi
 operation = st.selectbox(
     "Pilih operasi matematika:",
     ("Penjumlahan", "Pengurangan", "Perkalian", "Pembagian")
 )
 
-# Tombol untuk menghitung
+#Tombol untuk menghitung
 if st.button("Hitung"):
     if operation == "Penjumlahan":
         result = num1 + num2
